cc9b221f729e698ee7ae0118562f21e1abeaa9d5
Jim Liu <liujun@g2link.cn>
2017-12-13 15:55:36 +0800

    update

---
+++ springboot-async-sample/src/main/java/com/jim/async/SpringbootAsyncSampleApplication.java
@@ -0,0 +1,12 @@
+package com.jim.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAsyncSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAsyncSampleApplication.class, args);
+	}
+}

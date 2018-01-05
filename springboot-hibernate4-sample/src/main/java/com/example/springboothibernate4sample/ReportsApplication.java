7bb3b8818a4e293be275ed46ce627084c87046c2
liujun <liujun@g2link.cn>
2018-01-05 16:33:33 +0800

    hibernate4 init

---
+++ springboot-hibernate4-sample/src/main/java/com/example/springboothibernate4sample/ReportsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springboothibernate4sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsApplication.class, args);
+	}
+}
---
+++ springboot-hibernate4-sample/src/test/java/com/example/springboothibernate4sample/ReportsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboothibernate4sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

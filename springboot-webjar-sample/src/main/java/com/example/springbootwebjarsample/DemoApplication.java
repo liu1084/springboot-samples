ff7e6bc714c82ef651deb931dc246b55ec447526
liujun <liujun@g2link.cn>
2018-05-09 22:15:04 +0800

    add springboot-webjar-sample

---
+++ springboot-webjar-sample/src/main/java/com/example/springbootwebjarsample/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootwebjarsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example.springbootwebjarsample")
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ springboot-webjar-sample/src/main/java/com/example/springbootwebjarsample/IndexController.java
@@ -0,0 +1,11 @@
+package com.example.springbootwebjarsample;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping(value = {"", "/"})
+    public String index(){
+        return "index";
+    }
+}
